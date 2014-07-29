5cd30a32edc8a79c326b8e6058832ab4627f7d61
Shelby Kelly <shelby.kelly@colorado.edu>
2014-07-29 15:24:43 -0600

    Done with fruits

--- fruits.rb
+++ fruits.rb
@@ -1,1 +1,22 @@
 fruits = ['apples', 'oranges', 'bananas', 'pomegranates', 'grapes']
+
+puts fruits.count
+
+puts fruits[2]
+
+hash = Hash[fruits.map.with_index.to_a]
+puts hash['bananas']
+
+puts fruits.insert(1, 'rasberries')
+
+x = 0
+while x < 6
+  puts fruits[x].length
+  x = x + 1
+end
+
+puts fruits.each {|fruit| fruit.capitalize!}
+
+puts '*' * 20
+
+puts fruits.select {|g| g.match('g')}
